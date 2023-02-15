7b81bea7be66d78c9480968eb32b60a158f7e781
Dimitrio Mandamadiotis <me@dimitriosmandamadiotis.com>
2023-02-15 08:09:40 -0400

    Agregado enlace al github

--- ui/layouts/default.vue
+++ ui/layouts/default.vue
@@ -6,7 +6,7 @@
   <div class="py-12">
     <header>
       <nav class="navbar bg-base-100 justify-center">
-        <a class="btn btn-ghost normal-case text-xl">
+        <a class="btn btn-ghost normal-case text-xl" href="https://github.com/dimitrio-m/proyecto-de-grado">
           <icon name="mdi:github" />
         </a>
       </nav>
